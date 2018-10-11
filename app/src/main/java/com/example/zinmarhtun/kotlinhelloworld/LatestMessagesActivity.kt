7fd895e09c15efeee7daa7e183e07ff55e058546
Zin Mar Htun <zinmarhtun@reddotnetwork.com>
2018-10-11 14:32:28 +0630

    bugs fix

--- app/src/main/java/com/example/zinmarhtun/kotlinhelloworld/LatestMessagesActivity.kt
+++ app/src/main/java/com/example/zinmarhtun/kotlinhelloworld/LatestMessagesActivity.kt
@@ -1,15 +1,12 @@
 package com.example.zinmarhtun.kotlinhelloworld
 
 import android.content.Intent
+import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
-
-import kotlinx.android.synthetic.main.activity_latest_messages.*
 
 class LatestMessagesActivity : AppCompatActivity() {
 
@@ -20,7 +17,6 @@ class LatestMessagesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_latest_messages)
-        setSupportActionBar(toolbar)
 
         Log.d(TAG, "onCreate ...")
         verifyUserIsLoggedIn()
